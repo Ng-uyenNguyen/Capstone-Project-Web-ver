e9cde70e74469f724cb36e1f2b57cdc5784ecb9a
phuongnh2498 <phuongnh2498@gmail.com>
2022-03-09 12:50:31 +0700

    add dashboard

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Dashboard } from "../pages/Dashboard";
+import { Dashboard } from "../pages/dashboard/Dashboard";
 import { BlankPage } from "../pages/BlankPage";
 import { MainLayout } from "../layout/MainLayout";
 import { ManageSubject } from "../pages/ManageSubject";
 import { ManageClass } from "../pages/manage class/ManageClass";
-import  ManageTeacher  from "../pages/manage teacher/ManageTeacher";
+import ManageTeacher from "../pages/manage teacher/ManageTeacher";
 import { ClassDetail } from "../pages/manage class/ClassDetail";
 import { Login } from "../pages/login/Login";
 export const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login/>}/>
+        <Route path="/login" element={<Login />} />
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Dashboard />} />
           <Route path="/schedules" element={<BlankPage />} />
           <Route path="/teachers" element={<ManageTeacher />} />
-          <Route path="/students" element={<ManageTeacher/>} />
+          <Route path="/students" element={<ManageTeacher />} />
           <Route path="/classes" element={<ManageClass />} />
           <Route path="/specilizations" element={<personDetails />} />
           <Route path="/subjects" element={<ManageSubject />} />
